6d4cc5525e5a9ebc016217d6f3bd0da545daafd2
BMO <dic11249@gmail.com>
2020-03-09 22:41:04 +0800

    Initial commit

---
+++ src/main/java/com/bmo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.bmo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+        Person p = new Person();
+        p.hello();
+    }
+}
