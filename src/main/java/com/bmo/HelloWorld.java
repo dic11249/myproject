87b7202eaa8bf984080f1f7dac29135f2bccd797
BMO <dic11249@gmail.com>
2020-03-11 23:27:41 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/bmo/HelloWorld.java
+++ src/main/java/com/bmo/HelloWorld.java
@@ -5,5 +5,15 @@ public class HelloWorld {
 //        System.out.println("Hello");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = 'A';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Bmo";
     }
 }
--- src/main/kotlin/com/bmo/HelloWorld.kt
+++ src/main/kotlin/com/bmo/HelloWorld.kt
@@ -5,6 +5,10 @@ fun main() {
 //    Human().hello();
     val h = Human();
     h.hello();
+    val age : Int = 19;
+    var weight = 66.5f;
+    val name : String;
+    name = "Bmo";
 }
 
 class Human {
