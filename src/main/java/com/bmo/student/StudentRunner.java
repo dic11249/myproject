2ada7309a86df41206f2e291c98d67973a4b4811
BMO <dic11249@gmail.com>
2020-12-06 18:04:03 +0800

    static & companion object

--- src/main/java/com/bmo/student/StudentRunner.java
+++ src/main/java/com/bmo/student/StudentRunner.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class StudentRunner {
     public static void main(String[] args) {
 //        userInput();
+        com.bmo.kotlin.Student.getPass();
+        Student.pass = 40;
         Student stu = new Student("Bmo", 77, 60);
+        Student stu2 = new Student("Tom", 50, 40);
+        Student stu3= new Student("AJi", 30, 55);
         stu.print();
+        stu2.print();
+        stu3.print();
         System.out.println("High score: " + stu.highest());
     }
 
